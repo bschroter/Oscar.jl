e87e0705d3c3bc59c6ed25c9015c3094477b0e3e
Claus Fieker <claus.fieker@gmail.com>
2021-12-20 16:59:26 +0100

    Update experimental/GModule/GModule.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- experimental/GModule/GModule.jl
+++ experimental/GModule/GModule.jl
@@ -21,7 +21,7 @@ function GAP.gap_to_julia(::Type{QabElem}, a::GAP.GapObj) #which should be a Cyc
   return z
 end
 
-function GAP.gap_to_julia(::Type{QabElem}, a::Integer)
+function GAP.gap_to_julia(::Type{QabElem}, a::GAPInt)
   E = abelian_closure(QQ)[1]
   return E(fmpz(a))
 end
